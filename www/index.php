6d9fb57d2f619b9cd798c167ce94fcec51c80765
DimaRGB <dima.rgb@gmail.com>
2012-11-30 17:05:14 +0200

    Json result
    correct CSS

--- www/index.php
+++ www/index.php
@@ -1,19 +1,20 @@
 <html>
 <head>
+	<meta charset='utf-8' />
 	<script type='text/javascript' src='script/respondent.js'></script>
 	<link type='text/css' rel='stylesheet' href='css/respondent.css' />
 </head>
 <body>
-	<button id='nextButton' disabled='disabled' onclick='nextStep(event, this)'>Продовжити</button>
+	<button id='nextButton' disabled='disabled' onclick='clickButton(event, this)'>РџСЂРѕРґРѕРІР¶РёС‚Рё</button>
 	<table class='level' id='level2'>
 		<?php
 			$n = 7;
 			echo "<tr>";
 			for ($i = 0; $i < $n; $i++)
-				echo "<th class='gradeHead'>Погоджуюсь</th>";
+				echo "<th class='degreeHead' id='degreeHead2$i'>РџРѕРіРѕРґР¶СѓСЋСЃСЊ".(2*$i)."</th>";
 			echo "</tr><tr>";
 			for ($i = 0; $i < $n; $i++)
-				echo "<td class='grade' id='grade2$i' title='".($i+1)."' ondrop='drop(event, this)' ondragover='allowDrop(event)'></td>";
+				echo "<td class='degree' id='degree2$i' title='".(4*($i+1))."' ondrop='drop(event, this)' ondragover='allowDrop(event)'></td>";
 			echo "</tr>";
 		?>
 	</table>
@@ -22,19 +23,20 @@
 			$n = 3;
 			echo "<tr>";
 			for ($i = 0; $i < $n; $i++)
-				echo "<th class='gradeHead'>Погоджуюсь</th>";
+				echo "<th class='degreeHead' id='degreeHead1$i'>РџРѕРіРѕРґР¶СѓСЋСЃСЊ$i</th>";
 			echo "</tr><tr>";
 			for ($i = 0; $i < $n; $i++)
-				echo "<td class='grade' id='grade1$i' title='".(2*($i+1))."' ondrop='drop(event, this)' ondragover='allowDrop(event)'></td>";
+				echo "<td class='degree' id='degree1$i' title='".(4*($i+1))."' ondrop='drop(event, this)' ondragover='allowDrop(event)'></td>";
 			echo "</tr>";
 		?>
 	</table>
 	<table class='level' id='level0'>
-		<tr><th class='gradeHead'>Твердження</th></tr>
-		<tr><td class='grade' id='grade00' title='40' ondrop='drop(event, this)' ondragover='allowDrop(event)'>
+		<tr><th class='degreeHead'>РўРІРµСЂРґР¶РµРЅРЅСЏ</th></tr>
 		<?php
-			for ($i = 0; $i < 12; $i++)
-				echo "<div class='assertion' id='assertion$i' draggable='true' ondragstart='drag(event, this)'>assertion ".($i+1)."</div>";
+			$n = 6;
+			echo "<tr><td class='degree' id='degree00' title='$n' ondrop='drop(event, this)' ondragover='allowDrop(event)'>";
+			for ($i = 0; $i < $n; $i++)
+				echo "<div class='assertion' id='assertion$i' draggable='true' ondragstart='drag(event, this)'>".($i+1).". Assertion</div>";
 		?>
 		</td></tr>
 	</table>
--- www/script/respondent.js
+++ www/script/respondent.js
@@ -1,59 +1,60 @@
+function clickButton(e, ui) {
+	if (ui.id == 'nextButton') {
+		ui.id = 'okButton';
+		ui.disabled = 'disabled';
+		ui.innerHTML = 'Р—Р°РІРµСЂС€РёС‚Рё';
+		var level2 = document.getElementById('level2');
+		var level0 = document.getElementById('level0');
+		level2.style.display = 'table';
+		level0.style.display = 'none';
+	} else if (ui.id == 'okButton') {
+		var json = {"degree":[]};
+		count = parseInt(document.getElementById('degree00').title);
+		for (var i = 0; i < count; i++) {
+			var degree = document.getElementById('assertion' + i).parentNode;
+			json.degree[i] = parseInt(degree.id.substring('degree2'.length)) + 1;
+		}
+		alert(JSON.stringify(json));
+	}
+}
+
 function allowDrop(e, ui) {
 	e.preventDefault();
 }
 
 function drag(e, ui) {
-	var json = {"gradeId":"","assertionId":""};
-	json.gradeId = ui.parentNode.id;
+	var json = {"degreeId":"","assertionId":""};
+	json.degreeId = ui.parentNode.id;
 	json.assertionId = ui.id;
 	e.dataTransfer.setData('Text', JSON.stringify(json));
 }
 
 function drop(e, ui) {
 	e.preventDefault();
-	if (ui.className != 'grade' || ui.childElementCount >= ui.title) {
+	if (ui.className != 'degree' || ui.childElementCount >= ui.title)
 		return;
-	}
 	var data = e.dataTransfer.getData('Text');
 	var json = JSON.parse(data);
-	/*if (typeof JSON != "undefined")
-		json = JSON.parse(data);
-	else
-		json = eval('(' + data + ')');*/
-	if (json.gradeId != ui.id)
+	if (json.degreeId != ui.id)
 		ui.appendChild(document.getElementById(json.assertionId));
-	var nextButton = document.getElementById('nextButton');
-	if (nextButton.innerHTML == 'Продовжити') {
-		if (isLevelEmpty('level0'))
-			nextButton.disabled = ''; else
-		if (nextButton.disabled == '')
-			nextButton.disabled = 'disabled';
-	} else if (nextButton.innerHTML == 'Завершити') {
-		if (isLevelEmpty('level1'))
-			nextButton.disabled = ''; else
-		if (nextButton.disabled == '')
-			nextButton.disabled = 'disabled';
-	}
+
+	var button, level;
+	if ((button = document.getElementById('nextButton')) != null)
+		level = document.getElementById('level0');
+	else if ((button = document.getElementById('okButton')) != null)
+		level = document.getElementById('level1');
+
+	if (isLevelEmpty(level))
+		button.disabled = ''; else
+	if (button.disabled == '')
+		button.disabled = 'disabled';
 }
 
-function isLevelEmpty(levelId) {
-	greats = document.getElementById(levelId).rows[1].cells;
+function isLevelEmpty(level) {
+	greats = level.rows[1].cells;
 	for (var i = 0; i < greats.length; i++) {
 		if (greats[i].childElementCount > 0)
 			return false;
 	}
 	return true;
-}
-
-function nextStep(e, ui) {
-	if (ui.innerHTML == 'Продовжити') {
-		var level2 = document.getElementById('level2');
-		var level0 = document.getElementById('level0');
-		level2.style.display = 'block';
-		level0.style.display = 'none';
-		ui.disabled = 'disabled';
-		ui.innerHTML = 'Завершити';
-	} else if (ui.innerHTML == 'Завершити'){
-		alert('Тут сформувати Excel');
-	}
 }
