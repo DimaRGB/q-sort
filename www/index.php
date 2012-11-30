6db52de8bab67819415e4cca700ad252e1ef34bc
DimaRGB <dima.rgb@gmail.com>
2012-11-30 14:24:34 +0200

    New template; there are levels.
    Template based on <table> element.

--- www/index.php
+++ www/index.php
@@ -4,15 +4,39 @@
 	<link type='text/css' rel='stylesheet' href='css/respondent.css' />
 </head>
 <body>
-	<div id='block_dest1' class='block' title='4' ondrop='drop(event, this)' ondragover='allowDrop(event)'>
-	</div>
-	<div id='block_dest2' class='block' title='6' ondrop='drop(event, this)' ondragover='allowDrop(event)'>
-	</div>
-	<div id='block_source' class='block' title='40' ondrop='drop(event, this)' ondragover='allowDrop(event)'>
+	<button id='nextButton' disabled='disabled' onclick='nextStep(event, this)'>Продовжити</button>
+	<table class='level' id='level2'>
 		<?php
-			for ($i = 0; $i < 40; $i++)
-				echo "<div id='assertion$i' class='assertion' draggable='true' ondragstart='drag(event, this)'>source ".($i+1)."</div>";
+			$n = 7;
+			echo "<tr>";
+			for ($i = 0; $i < $n; $i++)
+				echo "<th class='gradeHead'>Погоджуюсь</th>";
+			echo "</tr><tr>";
+			for ($i = 0; $i < $n; $i++)
+				echo "<td class='grade' id='grade2$i' title='".($i+1)."' ondrop='drop(event, this)' ondragover='allowDrop(event)'></td>";
+			echo "</tr>";
 		?>
-	</div>
+	</table>
+	<table class='level' id='level1'>
+		<?php
+			$n = 3;
+			echo "<tr>";
+			for ($i = 0; $i < $n; $i++)
+				echo "<th class='gradeHead'>Погоджуюсь</th>";
+			echo "</tr><tr>";
+			for ($i = 0; $i < $n; $i++)
+				echo "<td class='grade' id='grade1$i' title='".(2*($i+1))."' ondrop='drop(event, this)' ondragover='allowDrop(event)'></td>";
+			echo "</tr>";
+		?>
+	</table>
+	<table class='level' id='level0'>
+		<tr><th class='gradeHead'>Твердження</th></tr>
+		<tr><td class='grade' id='grade00' title='40' ondrop='drop(event, this)' ondragover='allowDrop(event)'>
+		<?php
+			for ($i = 0; $i < 12; $i++)
+				echo "<div class='assertion' id='assertion$i' draggable='true' ondragstart='drag(event, this)'>assertion ".($i+1)."</div>";
+		?>
+		</td></tr>
+	</table>
 </body>
 </html>
--- www/script/respondent.js
+++ www/script/respondent.js
@@ -3,23 +3,57 @@ function allowDrop(e, ui) {
 }
 
 function drag(e, ui) {
-	e.dataTransfer.setData('Text', '{"blockId": "' + ui.parentNode.id + '", "assertionId": "' + ui.id + '"}');
+	var json = {"gradeId":"","assertionId":""};
+	json.gradeId = ui.parentNode.id;
+	json.assertionId = ui.id;
+	e.dataTransfer.setData('Text', JSON.stringify(json));
 }
 
 function drop(e, ui) {
 	e.preventDefault();
-	if (ui.className != 'block' || getAssertionCount(ui) >= ui.title)
+	if (ui.className != 'grade' || ui.childElementCount >= ui.title) {
 		return;
+	}
 	var data = e.dataTransfer.getData('Text');
-	var json;
-	if (typeof JSON != "undefined")
+	var json = JSON.parse(data);
+	/*if (typeof JSON != "undefined")
 		json = JSON.parse(data);
 	else
-		json = eval('(' + data + ')');
-	if (json.blockId != ui.id)
+		json = eval('(' + data + ')');*/
+	if (json.gradeId != ui.id)
 		ui.appendChild(document.getElementById(json.assertionId));
+	var nextButton = document.getElementById('nextButton');
+	if (nextButton.innerHTML == 'Продовжити') {
+		if (isLevelEmpty('level0'))
+			nextButton.disabled = ''; else
+		if (nextButton.disabled == '')
+			nextButton.disabled = 'disabled';
+	} else if (nextButton.innerHTML == 'Завершити') {
+		if (isLevelEmpty('level1'))
+			nextButton.disabled = ''; else
+		if (nextButton.disabled == '')
+			nextButton.disabled = 'disabled';
+	}
 }
 
-function getAssertionCount(block) {
-	return block.childElementCount;
+function isLevelEmpty(levelId) {
+	greats = document.getElementById(levelId).rows[1].cells;
+	for (var i = 0; i < greats.length; i++) {
+		if (greats[i].childElementCount > 0)
+			return false;
+	}
+	return true;
+}
+
+function nextStep(e, ui) {
+	if (ui.innerHTML == 'Продовжити') {
+		var level2 = document.getElementById('level2');
+		var level0 = document.getElementById('level0');
+		level2.style.display = 'block';
+		level0.style.display = 'none';
+		ui.disabled = 'disabled';
+		ui.innerHTML = 'Завершити';
+	} else if (ui.innerHTML == 'Завершити'){
+		alert('Тут сформувати Excel');
+	}
 }
